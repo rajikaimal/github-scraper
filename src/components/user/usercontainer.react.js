6dea8234063bc8427774f4f09dfa7a538ca4d312
rajikaimal <inbox.rajika@gmail.com>
2016-07-13 23:07:16 +0530

    comments for reference

--- src/components/user/usercontainer.react.js
+++ src/components/user/usercontainer.react.js
@@ -1,20 +1,27 @@
+//import react lib, child component, action, store
 import React from 'react';
 import usercontainerStore from '../../stores/user/usercontainerStore';
 import User from './user.react';
 import {Link} from 'react-router';
 
+//declare React component with createClass factory method
 const usercontainer = React.createClass({
+    //returns initial state of the component with getInitialState
     getInitialState: function () {
         return {
             user: {}
         };
     },
+    //load necessary resources when the component gets mounted to the DOM
     componentDidMount: function () {
         usercontainerStore.addChangeListener(this._onChange);
     },
+    //cleanup any resources or remove any listener
     componentWillUnmount: function () {
         usercontainerStore.removeChangeListener(this._onUnmount);
     },
+    //custom method definition
+    //callback for listener added in componentDidMount
     _onChange: function() {
         let userObj = usercontainerStore.retrieveUser();
         this.setState({
@@ -24,6 +31,7 @@ const usercontainer = React.createClass({
     _onUnmount: function() {
         
     },
+    //render function with JSX declaration of the component
     render: function () {
         let username = this.state.user.login;
         return (
@@ -35,6 +43,7 @@ const usercontainer = React.createClass({
                                 <Link to={`/user/${username}/repos`}> Repositories </Link> 
                                 <Link to={`/user/${username}/following`}> Following </Link>
                                 <Link to={`/user/${username}/followers`}> Followers </Link>
+                                <Link to={`/user/${username}/orgs`}> Organizations </Link>
                             </div>
                         : ''  
                 }
